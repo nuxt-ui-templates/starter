2e244c0aa39da613ef8e2abd5ad0dcec5ac1a381
Benjamin Canac <canacb1@gmail.com>
2026-09-15 12:30:08 +0200

    chore: add eslint-plugin-better-tailwindcss (#244)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import betterTailwindcss from 'eslint-plugin-better-tailwindcss'
+import { getDefaultAttributes } from 'eslint-plugin-better-tailwindcss/api/defaults'
 
 export default withNuxt(
-  // Your custom configs here
+  betterTailwindcss.configs['correctness-error'],
+  {
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'app/assets/css/main.css',
+        attributes: [
+          ...getDefaultAttributes(),
+          ['^v-bind:ui$', [{ match: 'objectValues' }]]
+        ]
+      }
+    }
+  }
 )
